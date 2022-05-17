1bae31e2ee5e0ad4c2727a06720f6332c1f39c53
tluwaniMS <tluwani2020@gmail.com>
2022-05-17 11:40:18 +0200

    adding-the-required-resolver-services-and-user-sample-data

---
+++ services/resolver-services.js
@@ -0,0 +1,39 @@
+const { users } = require("../sample-data/users-sample-data");
+
+const getAllUsers = () => {
+  return users;
+};
+
+const getUserByEmail = (args) => {
+  return users.filter((user) => user.email === args.email)[0];
+};
+
+const addUser = (args) => {
+  const newUser = {
+    firstName: args.firstName,
+    lastName: args.lastName,
+    email: args.email
+  };
+
+  users.push(newUser);
+
+  return `User with name ${newUser.firstName} has been created successfully`;
+};
+
+const deleteUserByEmail = (args) => {
+  const selectedUser = users.filter((user) => user.email === args.email)[0];
+  const userIndex = users.indexOf(selectedUser);
+  users.splice(userIndex, 1);
+
+  return `User has been successfuly deleted.`;
+};
+
+const updateUser = (args) => {
+  users.forEach((user) => {
+    user.email === args.email ? ((user.firstName = args.firstName), (user.lastName = args.lastName)) : "";
+  });
+
+  return `User has been updated successfuly.`;
+};
+
+module.exports = { getAllUsers, getUserByEmail, addUser, deleteUserByEmail, updateUser };
