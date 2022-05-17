644de9d87c383b82fd796415a6b83ae27df58434
tluwaniMS <tluwani2020@gmail.com>
2022-05-17 16:08:44 +0200

    adding-user-query-tests

--- tests/end-point-tests/user-end-point-tests.test.js
+++ tests/end-point-tests/user-end-point-tests.test.js
@@ -1,4 +1,31 @@
 const { app } = require("../../app");
 const request = require("supertest");
+const {
+  UPDATE_USER_BY_EMAIL_MUTATION_STRING,
+  DELETE_USER_BY_EMAIL_MUTATION_STRING,
+  ADD_USER_MUTATION_STRING
+} = require("../graphql-mutation-strings/UserGQLMutationString");
+const {
+  GET_ALL_USERS_QUERY_STRING,
+  GET_USER_BY_EMAIL_QUERY_STRING
+} = require("../graphql-query-strings/UserGQLQueryStrings");
 
-describe("Testing user gql end-point queries:", () => {});
+describe("Testing user gql end-point queries and mutations:", () => {
+  describe("Testing get-all-users gql query", () => {
+    it("It should return a status code 200", async () => {
+      const response = await request(app).post("/graphql").send({ query: GET_ALL_USERS_QUERY_STRING });
+
+      expect(response.status).toEqual(200);
+    });
+  });
+
+  describe("Testing get user by email gql query:", () => {
+    it("It should return a status code 200", async () => {
+      const response = await request(app)
+        .post("/graphql")
+        .send({ query: GET_USER_BY_EMAIL_QUERY_STRING, variables: { email: "dean@mock.com" } });
+
+      expect(response.status).toEqual(200);
+    });
+  });
+});
--- tests/graphql-query-strings/UserGQLQueryStrings.js
+++ tests/graphql-query-strings/UserGQLQueryStrings.js
@@ -1,6 +1,20 @@
-const GET_ALL_USERS_QUERY_STRING = ``;
+const GET_ALL_USERS_QUERY_STRING = `
+  query GetAllUsers {
+    getAllUsers{
+      email
+      firstName
+    }
+  }
+`;
 
-const GET_USER_BY_EMAIL_QUERY_STRING = ``;
+const GET_USER_BY_EMAIL_QUERY_STRING = `
+  query GetUserByEmail($email:String!){
+    getUserByEmail(email:$email){
+      email
+      firstName
+    }
+  }
+`;
 
 module.exports = {
   GET_ALL_USERS_QUERY_STRING,
