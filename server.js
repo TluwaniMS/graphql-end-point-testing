25c891152db343d7aec35543d4aff10bff35e82b
tluwaniMS <tluwani2020@gmail.com>
2022-05-17 13:33:27 +0200

    adding-required-server-config

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const { app } = require("./app");
 const port = process.env.PORT | 3000;
 
 app.listen(port, () => {
-  console.log(`server is running on port: ${port}`);
+  console.log(`server is running on port: http://localhost:${port}/graphql`);
 });
