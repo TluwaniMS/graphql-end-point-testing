6e283fac2aed97e033c24d79a11434cb9a09b015
tluwaniMS <tluwani2020@gmail.com>
2022-05-17 11:58:29 +0200

    adding-fix-to-gql-queries

--- graphql-queries/UserGQLQueries.js
+++ graphql-queries/UserGQLQueries.js
@@ -1,15 +1,16 @@
 const { GraphQLString, GraphQLList, GraphQLNonNull } = require("graphql");
 const { getAllUsers, getUserByEmail } = require("../services/resolver-services");
+const { UserModel } = require("../graphql-models/UserGQLModel");
 
 const getAllUsersQuery = {
-  type: new GraphQLList(UserType),
+  type: new GraphQLList(UserModel),
   resolve(parent, args) {
     return getAllUsers();
   }
 };
 
 const getUserByEmailQuery = {
-  type: UserType,
+  type: UserModel,
   args: {
     email: { type: new GraphQLNonNull(GraphQLString) }
   },
